e599139aa6e75c390b02c6fbe5f2cfdddeca185f
Joram Bosire <jbosire@vassar.edu>
2022-03-31 04:31:28 -0400

    Homework part 3 submission

--- Assignment-1/Part3/Queue.java
+++ Assignment-1/Part3/Queue.java
@@ -1,1 +1,69 @@
+import java.util.*;
+/**
+ * Write a description of class Queue here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Queue<E>
+{
+    // instance variables - replace the example below with your own
+    int size;
+    List<E> queue;
+
+    /**
+     * Constructor for objects of class Queue
+     */
+    public Queue()
+    {
+        this.queue = new ArrayList<>();
+        this.size = 0;
+    }
+
+    public void enqueue(E elem){
+        queue.add(0,elem);
+        size++;
+    }
+    
+    public E dequeue(){
+        if(size == 0){
+            return null;
+        }
+        
+        E res = queue.get(size - 1);
+        queue.remove(size - 1);
+        size--;
+        return res;
+    }
+    
+    public E rear(){
+        if(this.size == 0){
+            return null;
+        }
+        
+        return queue.get(0);
+    }
+    
+    public E front(){
+        if(this.size == 0){
+            return null;
+        }
+        
+        return queue.get(size - 1);
+    }
+    
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public int size(){
+        return size;
+    }
+    
+    
+}
 
--- Assignment-1/Part3/Stack.java
+++ Assignment-1/Part3/Stack.java
@@ -1,1 +1,54 @@
+import java.util.*;
 
+public class Stack<E>
+{
+    // instance variables - replace the example below with your own
+    int size;
+    List<E> stack;
+
+    
+    public Stack()
+    {
+        // initialise instance variables
+        this.stack = new ArrayList<>();
+        this.size = 0;
+    }
+
+    public void push(E elem){
+        this.stack.add(0,elem);
+        this.size++;
+    }
+    
+    public E pop(){
+        if(size == 0){
+            return null;
+        }
+        
+        E res = stack.get(0);
+        stack.remove(0);
+        size--;
+        return res;
+    }
+    
+    public E top(){
+        if(this.size == 0){
+            return null;
+        }
+        
+        return stack.get(0);
+    }
+    
+    public boolean isEmpty(){
+        if(this.size == 0){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public int size(){
+        return this.size;
+    }
+    
+    
+}
